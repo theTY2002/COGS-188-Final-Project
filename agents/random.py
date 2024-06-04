ebe24f2cc11ef5295e296323f59aeb74abffcdb6
Kevin Fisher <kfish610@gmail.com>
2024-06-03 21:44:47 -0700

    Add random agent

---
+++ agents/random.py
@@ -0,0 +1,11 @@
+import random
+from simulator.agent import Agent
+from simulator.melds import Meld
+from simulator.tiles import Tile
+
+class RandomAgent(Agent):
+    def choose_discard(self, hand: list[Tile], melds: list[list[Meld]], player: int) -> int:
+        return random.randrange(len(hand))
+    
+    def choose_meld(self, available_melds: list[Meld], hand: list[Tile], melds: list[list[Meld]], player: int) -> int | None:
+        return random.choice([None] + list(range(len(available_melds))))
