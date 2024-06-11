b153a360cb4a7cb9b1fb925dd032ad4862e07aef
theTY2002 <trevoryip2002@gmail.com>
2024-06-11 03:22:04 -0700

    More Bug Fixing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import torch
 from agents.random import RandomAgent
 from learning.agent import DQNAgent
 from simulator.game import Game
@@ -10,3 +11,6 @@ if game.winner == 3:
     agent.end_game(100)
 else:
     agent.end_game(0)
+
+torch.save(agent.discard_trainer.qnetwork_local.state_dict(), 'discard_checkpoint.pth')
+torch.save(agent.meld_trainer.qnetwork_local.state_dict(), 'meld_checkpoint.pth')
